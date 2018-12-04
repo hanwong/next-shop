b845d410e39d9354b25dbba6310bedae7ec5dedc
hanwong <hanationbear@gmail.com>
2018-12-04 12:12:35 +0900

    gql update, delete item

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -5,10 +5,22 @@ const Mutations = {
         ...args
       }
     }, info);
-
-    console.log(item);
-
     return item;
+  },
+  updateItem(parent, args, ctx, info) {
+    const updates = { ...args };
+    delete updates.id;
+    return ctx.db.mutation.updateItem({
+      data: updates,
+      where: {
+        id: args.id
+      }
+    }, info);
+  },
+  async deleteItem(parent, args, ctx, info) {
+    const where = { id: args.id };
+    const item = await ctx.db.query.item({ where }, info);
+    return ctx.db.mutation.deleteItem({ where }, info);
   }
 };
 

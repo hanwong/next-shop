575eccccc917449fb8adb24e35e45af727fd65fd
hanwong <hanationbear@gmail.com>
2018-12-05 16:52:56 +0900

    gql signup
    `

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,8 +1,15 @@
+const cookieParser = require('cookie-parser');
+
 require('dotenv').config({ path: 'variables.env'});
 const createServer = require('./createServer');
 const db = require('./db');
 
 const server = createServer();
+
+// for JWT
+server.express.use(cookieParser());
+
+// 
 
 server.start({
   cors: {
--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,6 @@
+const bycrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 const Mutations = {
   async createItem(parent, args, ctx, info) {
     const item = await ctx.db.mutation.createItem({
@@ -21,6 +24,29 @@ const Mutations = {
     const where = { id: args.id };
     const item = await ctx.db.query.item({ where }, info);
     return ctx.db.mutation.deleteItem({ where }, info);
+  },
+  async signup(parent, args, ctx, info)  {
+    // lowercase their email
+    args.email = args.email.toLowerCase();
+    // hashing password
+    const password = await bycrypt.hash(args.password, 10);
+    // create user
+    const user = await ctx.db.mutation.createUser({
+      data: {
+        ...args,
+        password,
+        permissions: { set: ['USER'] }
+      }
+    }, info);
+    // create jwt token
+    const token = jwt.sign({userId: user.id}, process.env.APP_SECRET);
+    // set jwt as cookie on the response
+    ctx.response.cookie('token', token, {
+      httpOnly: true,
+      maxAge: 1000*60*60*24*365 // 1 year
+    });
+    // return user to the browser
+    return user;
   }
 };
 

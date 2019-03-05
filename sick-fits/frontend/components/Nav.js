46523ba9455741f5bc8d12db857d5a8220c19333
hanwong <hanationbear@gmail.com>
2019-03-05 22:46:15 +0900

    get user data

--- sick-fits/frontend/components/Nav.js
+++ sick-fits/frontend/components/Nav.js
@@ -1,8 +1,15 @@
 import Link from 'next/link';
 import NavStyles from './styles/NavStyles';
+import User from './User';
 
 const Nav =_=> (
   <NavStyles>
+    <User>
+      {(data) => {
+        console.log(data);
+        return <p>User</p>;
+      }}
+    </User>
     <Link href="/">
       <a>Shop</a>
     </Link>

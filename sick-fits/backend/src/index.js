5bb8f6434be9d13e4b577caf66361823553de1ac
hanwong <hanationbear@gmail.com>
2019-03-05 22:45:40 +0900

    return jwt token

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,4 +1,5 @@
 const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
 
 require('dotenv').config({ path: 'variables.env'});
 const createServer = require('./createServer');
@@ -9,7 +10,15 @@ const server = createServer();
 // for JWT
 server.express.use(cookieParser());
 
-// 
+// decode the JWT so we can get the user Id on each request
+server.express.use((req, res, next) => {
+  const { token } = req.cookies;
+  if (token) {
+    const { userId } = jwt.verify(token, process.env.APP_SECRET);
+    req.userId = userId;
+  }
+  next();
+});
 
 server.start({
   cors: {
